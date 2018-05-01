0b64053ed8288e3ebefbaeb2edf3bc423dbc636f
Dawid <avix133@gmail.com>
2018-05-02 00:25:55 +0200

    datepicker

--- routes/cars.js
+++ routes/cars.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
-/* GET users listing. */
+
+/* GET home page. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+
+	res.render('cars', { title: 'CARS!' });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,18 @@ var models = require('../models');
 /* GET home page. */
 router.get('/', function(req, res) {
 	models.car.findAll().then(cars => {
-		console.log("Here: " + cars[0].reg_no);
+		for (var i = 0; i < cars.length; i++) {
+			console.log("Here: " + cars[i].reg_no);
+		}
 	}
 	);
-	res.render('index', { title: 'Express' });
+	res.render('index', { title: 'CarRental' });
 });
+
+// router.get('/cars', function(req, res) {
+// 	res.render('index', { title: 'CARS' });
+// });
+
+router.use('/cars', require('./cars'));
 
 module.exports = router;
